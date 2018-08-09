25f1fc7ffea85027aaf3765ade3fcafe07f7ed0f
Aleksandr Medvedev <onl1ner.as@gmail.com>
2018-08-09 14:02:04 +1000

    account repository - allows to load all accounts from db without pagination

--- repository/account.go
+++ repository/account.go
@@ -1,6 +1,9 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+	"github.com/messwith/coding_challenge/models"
+)
 
 type AccountRepository struct {
 	db *sql.DB
@@ -8,6 +11,30 @@ type AccountRepository struct {
 
 func NewAccountRepository(db *sql.DB) *AccountRepository {
 	return &AccountRepository{db: db}
+}
+
+func (ar *AccountRepository) GetAccounts() ([]models.Account, error) {
+
+	accounts := []models.Account{}
+
+	rows, err := ar.db.Query(`SELECT id, owner, balance, currency FROM accounts`)
+	if err == sql.ErrNoRows {
+		return accounts, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		account := models.Account{}
+		err = rows.Scan(&account.Id, &account.Owner, &account.Balance, &account.Currency)
+		if err != nil {
+			return nil, err
+		}
+		accounts = append(accounts, account)
+	}
+	return accounts, nil
 }
 
 
